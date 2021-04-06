77f33de0dc9c075dbea7e04fd858ded6d3ff1054
Universe <bigtree123433@gmail.com>
2021-04-06 15:29:54 +0900

    .

--- net_cifar.py
+++ net_cifar.py
@@ -139,7 +139,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=10, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None, stem=None, dct_ratio_low=0.0, dct_ratio_high=1.0):
+                 norm_layer=None, stem=None):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -158,10 +158,10 @@ class ResNet(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        # self.stem = stem(3, self.inplanes, norm_layer)
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
+        self.stem = stem(3, self.inplanes, norm_layer)
+        # self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        # self.bn1 = norm_layer(self.inplanes)
+        # self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
@@ -216,10 +216,10 @@ class ResNet(nn.Module):
 
     def _forward_impl(self, x):
         # See note [TorchScript super()]
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        # x = self.stem(x)
+        # x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        x = self.stem(x)
 
         x = self.layer1(x)
         x = self.layer2(x)
@@ -249,7 +249,7 @@ class AdvResNet(ResNet):
                  attack_mode='pgd'):
         super().__init__(block, layers, num_classes=num_classes, zero_init_residual=zero_init_residual,
                  groups=groups, width_per_group=width_per_group, replace_stride_with_dilation=replace_stride_with_dilation,
-                 norm_layer=norm_layer, stem=stem, dct_ratio_low=0.0, dct_ratio_high=1.0)
+                 norm_layer=norm_layer, stem=stem)
         self.attacker = attacker
         self.mixbn = False
         self.attack_mode = attack_mode
