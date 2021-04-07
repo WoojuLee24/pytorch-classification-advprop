809735ef9619ea042f9d65ba068ea8a19baa98bd
Universe <bigtree123433@gmail.com>
2021-04-07 11:06:31 +0900

    main corruption test

--- main.py
+++ main.py
@@ -606,7 +606,7 @@ def test_c(test_data, model, criterion, epoch, use_cuda):
     for corruption in CORRUPTIONS:
         # Reference to original data is mutated
         test_data.data = np.load(os.path.join(args.data + "-c", corruption + '.npy'))
-        test_data.targets = torch.LongTensor(np.load(os.path.join(args.data, "cifar10-c", 'labels.npy')))
+        test_data.targets = torch.LongTensor(np.load(os.path.join(args.data + "-c", 'labels.npy')))
 
         test_loader = torch.utils.data.DataLoader(
             test_data,
